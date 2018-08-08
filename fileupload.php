fdac513a5adb9d1d6a6f5338b4cc87f0e975faf3
SHSSCPG131 <SHSSCPG131@gmail.com>
2018-08-08 21:23:58 +0530

    File algorthim updated

---
+++ fileupload.php
@@ -0,0 +1,13 @@
+<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="upload.php" method="POST" enctype="multiplart/form-data">
+<input type="file" name= "file" >
+<button type="submit" name="subimt"> UPLOAD</button>
+</body>
+</html>
+?>
---
+++ upload.php
@@ -0,0 +1,36 @@
+<?php
+if(isset($_POST['submit'])){
+	$file=$_FILES['file'];
+
+	$filename=$_FILES['file']['name'];
+	$fileTmpName=$_FILES['file']['tmp_name'];
+	$fileSize=$_FILES['file']['size'];
+	$fileError=$_FILES['file']['error'];
+	$fileType=$_FILES['file']['type'];
+
+	$fileExt = explode('.', $filename);
+	$fileActualExt=strtolower(end($fileExt));
+	$allowed = array('jpg','jpeg','png');
+	if(in_array($fileActualExt,$allowed)){
+		if($fileError === 0){
+			if($fileSize<1000000){
+				//Passed cases
+					$filenamenew = uniqid('',true).".".$fileActualExt;
+					$fileDest='uploads/'.$filenamenew;
+					move_uploaded_file($fileTmpName, $fileDest);
+					header("Location: index.php?uploadsucess");
+			}
+			else{
+				echo "Your file is too big!";
+			}
+		}
+		else{
+			echo "There was an error uploading your file!";
+		}
+
+	}
+	else{
+		echo "You cannot upload file of this type!";
+	}
+}
+?>
